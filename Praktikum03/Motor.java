cf26302d9e802ccd647ab8140fc57457dc3aabeb
yayaluft <syanuha66@gmail.com>
2026-09-07 08:56:26 +0700

    Percobaan 2 – Enkapsulasi

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -1,14 +1,42 @@
 package Praktikum03;
 
 public class Motor {
-    public String platNomor;
-    public boolean statusMesin;
-    public int kecepatan;
-
+    private String platNomor;
+    private boolean statusMesin;
+    private int kecepatan;
+    
     public void displayInfo() {
         System.out.println("Plat Nomor: " + this.platNomor);
         System.out.println("Status Mesin: " + (this.statusMesin ? "On" : "Off"));
         System.out.println("Kecepatan: " + this.kecepatan);
-        System.out.println("===================-======");
+        System.out.println("=========================");
+    }
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+
+    public void setStatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        if(!this.statusMesin && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            this.kecepatan = kecepatan;
+        }
     }
 }
